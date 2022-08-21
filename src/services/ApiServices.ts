35e40feec1cec42a9d68996d6289192630971760
Wilfried AGO <wilfriedago@pm.me>
2022-08-21 17:33:50 +0100

    Mise à jour des liens

--- src/services/ApiServices.ts
+++ src/services/ApiServices.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 // Chargement de l'apiKey depuis les variables d'environnement
-const apiKey: string = process.env.REACT_APP_APIKEY as string;
+const apiKey: string = process.env.REACT_APP_API_KEY as string;
 
 // Configuration de base d'axios
 const api = axios.create({
-  baseURL: "https://api.countrystatecity.in/v1/countries/",
+  baseURL: "https://api.countrystatecity.in/v1/countries",
   timeout: 5000,
   headers: { "X-CSCAPI-KEY": apiKey },
 });
@@ -22,16 +22,7 @@ export const getAllCountries = async () => {
 // Récupération de la liste de tout les départements par pays
 export const getStateByCountry = async (country: string) => {
   try {
-    return await api.get(`/`);
-  } catch (error) {
-    console.error("error", error);
-  }
-};
-
-// Récupération de la liste des villes par départements
-export const getCitiesByState = async (state: string) => {
-  try {
-    return await api.get(`/`);
+    return await api.get(`/${country}/states`);
   } catch (error) {
     console.error("error", error);
   }
@@ -40,16 +31,16 @@ export const getCitiesByState = async (state: string) => {
 // Récupération de la liste des villes par pays
 export const getCitiesByCountry = async (country: string) => {
   try {
-    return await api.get(`/`);
+    return await api.get(`/${country}/cities`);
   } catch (error) {
     console.error("error", error);
   }
 };
 
 // Récupération de la liste des villes par département et par pays
-export const getCitiesByStateAndCountry = async (state: string, country: string) => {
+export const getCitiesByCountryAndState = async (country: string, state: string) => {
   try {
-    return await api.get(`/`);
+    return await api.get(`/${country}/states/${state}/cities`);
   } catch (error) {
     console.error("error", error);
   }
