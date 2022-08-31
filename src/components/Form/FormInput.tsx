eddd761291f27984061d5d46639c269dcfa03225
Wilfried AGO <wilfriedago@pm.me>
2022-08-31 17:22:55 +0100

    Changement de l'évènement

--- src/components/Form/FormInput.tsx
+++ src/components/Form/FormInput.tsx
@@ -1,13 +1,13 @@
 import { FormInputProps } from "../../interfaces";
 import Datalist from "./Datalist";
 
-const FormInput = ({ name, label, datalistID, datalist, onChange }: FormInputProps) => {
+const FormInput = ({ name, label, datalistID, datalist, onBlur }: FormInputProps) => {
   return (
     <div className="flex flex-col mx-2">
       <label className="ml-2" htmlFor={name}>
         {label}
       </label>
-      <input type="text" className="input" id={name} name={name} list={datalistID} onChange={onChange} required />
+      <input type="text" className="input" id={name} name={name} list={datalistID} onBlur={onBlur} required />
       <Datalist id={datalistID} list={datalist} />
     </div>
   );
